da681936ddcf5972bf12a8853ffd4ddddc4cf68c
2819685584 <2819685584@qq.com>
2024-09-13 17:07:38 +0800

    Site updated: 2024-09-13 17:07:38

--- js/copyPrompt.js
+++ js/copyPrompt.js
@@ -1,25 +1,33 @@
 document.addEventListener("copy", function () {
-  Vue.prototype.$notify({
-    title: "复制成功",
-    message: "转载请遵守CC协议",
-    position: "top-left",
-    offset: 50,
-    showClose: true,
-    type: "success",
-    duration: 4000,
+  new Vue({
+    data: function () {
+      this.$notify({
+        title: "复制成功",
+        message: "转载请遵守CC协议",
+        position: "top-left",
+        offset: 50,
+        showClose: true,
+        type: "success",
+        duration: 4000,
+      });
+    },
   });
 });
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "F12") {
-    Vue.prototype.$notify({
-      title: "逮住😜",
-      message: "小兄弟，扒源请遵循GPL协议！",
-      position: "top-left",
-      offset: 50,
-      showClose: true,
-      type: "warning",
-      duration: 4000,
+    new Vue({
+      data: function () {
+        this.$notify({
+          title: "逮住😜",
+          message: "小兄弟，扒源请遵循GPL协议！",
+          position: "top-left",
+          offset: 50,
+          showClose: true,
+          type: "warning",
+          duration: 4000,
+        });
+      },
     });
   }
 });
