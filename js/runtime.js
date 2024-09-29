8c78557c4278b037ba15fe9377900dadb459e457
2819685584 <2819685584@qq.com>
2024-09-29 17:21:26 +0800

    Site updated: 2024-09-29 17:21:26

--- js/runtime.js
+++ js/runtime.js
@@ -1,29 +1,16 @@
-var now = new Date(); // 当前时间
-var birthDate = new Date("2000-01-07"); // 你的出生日期
-
-function calculateDaysLived() {
-  // 计算两个日期之间的差异（毫秒）
-  var timeDiff = now - birthDate;
-  
-  // 将差异转换为天数
-  var diffDays = timeDiff / (1000 * 60 * 60 * 24);
-  
-  // 获取整数天数
-  var daysLived = Math.floor(diffDays);
-  
-  // 输出结果
-  let content = `<div style="font-size:13px;font-weight:bold">
-    从出生到现在已经活了 ${daysLived} 天
-  </div>`;
-
-  // 更新页面上的内容
-  if (document.getElementById("workboard")) {
-    document.getElementById("workboard").innerHTML = content;
-  }
+var now = new Date();
+var year, month, week, date, dates, weekStr, monthStr;
+var asideTime, asideDay, asideDayNum;
+var animalYear, ganzhiYear, lunarMon, lunarDay;
+cardTimes();
+// 刷新时钟时间
+function cardRefreshTimes() {
+    var a_t_r = document.getElementById("aside-time-right");
+    if(a_t_r)a_t_r.innerHTML = now.getHours().toString().padStart(2, '0') + ":" + now.getMinutes().toString().padStart(2, '0') + ":" + now.getSeconds().toString().padStart(2, '0');
 }
-
-// 每天更新一次内容
+// 设置重复执行函数，周期1000ms
 setInterval(() => {
-  now = new Date(); // 更新当前时间
-  calculateDaysLived();
-}, 86400000); // 86400000 毫秒 = 1 天
+    cardRefreshTimes();
+}, 1000);
+
+
